2c4295788e35700b7779e0285bbca5c3122a9710
antorrg <114789901+antorrg@users.noreply.github.com>
2024-02-17 22:03:05 -0300

    Limpiando Error.jsx

--- client/src/views/Error.jsx
+++ client/src/views/Error.jsx
@@ -1,19 +1,11 @@
 import style from './styles/Error.module.css'
-// import { useAuth } from "../Auth/AuthContext/AuthContext";
-import GenericButton from "../components/Buttons/GenericButton";
+
 
 const Error = () => {
-  // const { logout } = useAuth();
-
-  // const goBack = () => {
-
-  //     logout(); // Llamar a la función de logout
-  // };
-
   
   return (
       <div className={style.error}>
-      <h1 className={style.cube}>Error 404</h1>
+      <h1>Error 404</h1>
       <h2>Page not found</h2>
       
       </div>
